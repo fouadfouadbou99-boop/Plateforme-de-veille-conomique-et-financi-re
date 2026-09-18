6fcf04c0b954ee44dacae4aada202be7d5973cf6
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-09-18 13:35:35 +0100

    Add initial Streamlit app for news display

---
+++ Actualites.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import streamlit as st
+
+st.title("📰 Actualités")
+
+df = pd.DataFrame(
+    columns=[
+        "source",
+        "title",
+        "category",
+        "publication_date",
+    ]
+)
+
+st.dataframe(
+    df,
+    width="stretch"
+)

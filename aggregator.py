b90b123bb347153d86ec842f0e2ae1d5dbdacf9e
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-09-21 11:39:35 +0100

    Remove OECD documents from aggregator

--- aggregator.py
+++ aggregator.py
@@ -1,9 +1,10 @@
+# connectors/aggregator.py
+
 from connectors.bam import get_bam_documents
 from connectors.hcp import get_hcp_documents
 from connectors.mef import get_mef_documents
 from connectors.imf import get_imf_documents
 from connectors.worldbank import get_worldbank_documents
-from connectors.oecd import get_oecd_documents
 
 
 def get_all_documents():
@@ -15,6 +16,5 @@ def get_all_documents():
     docs.extend(get_mef_documents())
     docs.extend(get_imf_documents())
     docs.extend(get_worldbank_documents())
-    docs.extend(get_oecd_documents())
 
     return docs

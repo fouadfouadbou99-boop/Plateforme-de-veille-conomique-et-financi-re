55c83a0a33828527e28554cccf5cf4ca190861e1
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-09-21 11:31:34 +0100

    Add aggregator.py to fetch documents from multiple sources

---
+++ aggregator.py
@@ -0,0 +1,20 @@
+from connectors.bam import get_bam_documents
+from connectors.hcp import get_hcp_documents
+from connectors.mef import get_mef_documents
+from connectors.imf import get_imf_documents
+from connectors.worldbank import get_worldbank_documents
+from connectors.oecd import get_oecd_documents
+
+
+def get_all_documents():
+
+    docs = []
+
+    docs.extend(get_bam_documents())
+    docs.extend(get_hcp_documents())
+    docs.extend(get_mef_documents())
+    docs.extend(get_imf_documents())
+    docs.extend(get_worldbank_documents())
+    docs.extend(get_oecd_documents())
+
+    return docs

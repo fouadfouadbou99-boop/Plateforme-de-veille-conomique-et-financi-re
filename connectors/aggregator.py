15c2ca5b7fe23e3e415f64e81aabc883a632fe10
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-09-22 15:16:15 +0100

    Implement error handling for document fetching
    Added error handling for document retrieval from various sources.

--- connectors/aggregator.py
+++ connectors/aggregator.py
@@ -1,4 +1,6 @@
 from connectors.bam import get_bam_documents
+from connectors.hcp import get_hcp_documents
+from connectors.mef import get_mef_documents
 from connectors.imf import get_imf_documents
 from connectors.worldbank import get_worldbank_documents
 
@@ -7,8 +9,29 @@ def get_all_documents():
 
     docs = []
 
-    docs.extend(get_bam_documents())
-    docs.extend(get_imf_documents())
-    docs.extend(get_worldbank_documents())
+    try:
+        docs.extend(get_bam_documents())
+    except Exception as e:
+        print(e)
+
+    try:
+        docs.extend(get_hcp_documents())
+    except Exception as e:
+        print(e)
+
+    try:
+        docs.extend(get_mef_documents())
+    except Exception as e:
+        print(e)
+
+    try:
+        docs.extend(get_imf_documents())
+    except Exception as e:
+        print(e)
+
+    try:
+        docs.extend(get_worldbank_documents())
+    except Exception as e:
+        print(e)
 
     return docs
